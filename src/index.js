5a883a469dd4c367c5568558cac4d94dac78d08c
RetaxMaster <retaxmaster@hotmail.com>
2021-01-18 19:18:42 -0600

    API de internacionalizacion

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@
  const baseUrl = "https://platzi-avo.vercel.app";
  const appNode = document.querySelector("#app")
 
+ // Intl
+ // 1.- Format fechas
+ // 2.- Format monedas
+
+ const formatPrice = price => {
+
+    const newPrice = new window.Intl.NumberFormat("es-MX", {
+        style: "currency",
+        currency: "MXN"
+    }).format(price)
+
+    return newPrice;
+ }
+
 // web api
 
 // Conectarnos al server
@@ -21,18 +35,33 @@ window
             // Crear imagen
             const imagen = document.createElement("img");
             imagen.src = `${baseUrl}${item.image}`;
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
 
             // Crear título
             const title = document.createElement("h2");
+            title.className = "text-lg"
             title.textContent = item.name;
 
             // Crear precio
             const price = document.createElement("div");
-            price.textContent = item.price;
+            price.className = "text-gray-600"
+            price.textContent = formatPrice(item.price);
 
+            // Creamos un contenedor el título y el precio
+            const priceAndTitle = document.createElement("div")
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
+
+            // Metemos todo dentro de una tarjeta contenedora
+            const card = document.createElement("div");
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.append(imagen, priceAndTitle);
+
+            // Metemos todo dentro del contenedor principal
             const contenedor = document.createElement("div");
-            contenedor.append(imagen, title, price)      ;
-            
+            contenedor.appendChild(card);
+
             todosLosItems.push(contenedor);
 
         });
