8b8b640e6b40bee604b5a300b8877b3a1ec601f9
RetaxMaster <retaxmaster@hotmail.com>
2021-01-18 21:06:07 -0600

    Agregado de las imagenes de forma dinamica desde JavaScript

--- workshop-2/src/index.js
+++ workshop-2/src/index.js
@@ -3,4 +3,37 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+ const minimum = 1;
+ const maximum = 122;
+ const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const imagen = document.createElement("img");
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`
+
+    container.appendChild(imagen);
+
+    return container;
+
+}
+
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+const addImage = () => {
+
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+    
+}
+
+addButton.addEventListener("click", addImage)
